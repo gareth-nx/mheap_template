14678f6e6a1eafad1af97b373e805977331ef11e
gareth <gareth@gareth-P410.com>
2022-12-21 13:35:26 +1100

    rename

--- example_usage.f90
+++ example_usage.f90
@@ -1,5 +1,6 @@
 ! Copyright (c) 2014, Daniel Pena 
 ! All rights reserved.
+!
 
 ! Redistribution and use in source and binary forms, with or without
 ! modification, are permitted provided that the following conditions are met:
@@ -22,22 +23,22 @@
 ! SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 module nodedata
-    ! Define the "type" of data that the heap holds, and 
-    ! a function IS_LESS_THAN(a,b) where a,b are heap entries
-    implicit none
-    integer, parameter :: dp = selected_real_kind(12)
+! Define the "type" of data that the heap holds, and 
+! a logical function to determine if a < b (where a,b are heap data entries)
+implicit none
+integer, parameter :: dp = selected_real_kind(12)
+! Type held in our heap
+type :: point3
+    real(dp) :: x(3)     
+end type
 
-    type :: point3
-        real(dp) :: x(3)     
-    end type
+contains
 
-    contains
-
-    logical function less_than(a, b)
-        type(point3), intent(in) :: a, b
-        less_than = norm2(a%x) < norm2(b%x)
-    end function
-end module
+logical function less_than(a, b)
+    type(point3), intent(in) :: a, b
+    less_than = norm2(a%x) < norm2(b%x)
+end function
+end module nodedata
 
 module mheap_point3
 ! Create a type THEAP containing data of type(NODE_DATA_TYPE)
